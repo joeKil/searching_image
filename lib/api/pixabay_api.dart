edf4be149ee2a30f73e4678f3637ce60ca8d5cf8
joGIL <87177737+solinGIL@users.noreply.github.com>
2023-06-28 05:24:54 +0900

    Update pixabay_api.dart

--- lib/api/pixabay_api.dart
+++ lib/api/pixabay_api.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:searching_image/model/pixabay_dto.dart';
 
 Future<List<PixabayDTO>> fetchPhotos(String query) async {
-  final response = await http.get(Uri.parse('https://pixabay.com/api/?key=37933558-33cf47fb6f41d74a8d0182295&q=$query'));
+  final response = await http.get(Uri.parse('https://pixabay.com/api/?key=api키넣는자리=$query'));
   Map<String, dynamic> jsonResponse = json.decode(response.body);
   List<dynamic> hits = jsonResponse['hits'];
   return hits.map((item) => PixabayDTO.fromJson(item)).toList();
